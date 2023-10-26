20dbe6ecaf7426022f5e83f5a6a2085322efb5a0
Alex Wulf <alexaethelwulf@gmail.com>
2023-10-26 14:30:35 -0500

    Navigation Sidebar

--- components/FileUpload.tsx
+++ components/FileUpload.tsx
@@ -30,7 +30,6 @@ export const FileUpload = ({
 				/>
 
 				<button
-					title='Remove Image'
 					onClick={() => onChange('')}
 					type='button'
 					className='bg-rose-500 text-white p-1 rounded-full absolute top-0 right-0 shadow-sm'
