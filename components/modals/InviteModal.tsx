054599fda8a637406556c0262a41ea7c4206982f
Alex Wulf <alexaethelwulf@gmail.com>
2023-10-27 23:43:05 -0500

    Invitations

--- components/modals/InviteModal.tsx
+++ components/modals/InviteModal.tsx
@@ -1,6 +1,10 @@
 'use client'
 
+import { useState } from 'react'
+import axios 				from 'axios'
+
 import {
+	Check			,
 	Copy			,
 	RefreshCw	,
 } from 'lucide-react'
@@ -18,10 +22,9 @@ import {
 import { Button } from '@/components/ui/Button'
 import { Input } 	from '@/components/ui/Input'
 import { Label } 	from '@/components/ui/Label'
-import { useState } from 'react'
 
 export const InviteModal = () => {
-	const { isOpen, onClose, type, data } = useModalStore()
+	const { onOpen, isOpen, onClose, type, data } = useModalStore()
 
 	const origin = useOrigin()
 
@@ -37,6 +40,26 @@ export const InviteModal = () => {
 
 	const onCopy = () => {
 		navigator.clipboard.writeText(inviteUrl)
+
+		setCopied(true)
+
+		setTimeout(() => {
+			setCopied(false)
+		}, 1000)
+	}
+
+	const onNew = async () => {
+		try {
+				setIsLoading(true)
+
+				const response = await axios.patch(`/api/servers/${server?.id}/invite-code`)
+
+				onOpen('invite', { server: response.data })
+		} catch (error) {
+				console.log(error)
+		} finally {
+				setIsLoading(false)
+		}
 	}
 
 	return (
@@ -57,17 +80,28 @@ export const InviteModal = () => {
 
 					<div className='flex items-center mt-2 gap-x-2'>
 						<Input
+							disabled={isLoading}
 							readOnly
 							value={inviteUrl}
 							className='bg-zinc-300/50 border-0 focus-visible:ring-0 text-black focus-visible:ring-offset-0'
 						/>
 
-						<Button size='icon'>
-							<Copy className='w-4 h-4' />
+						<Button
+							disabled={isLoading}
+							onClick={onCopy}
+							size='icon'
+						>
+							{copied ? (
+								<Check className='w-4 h-4' />
+							) : (
+								<Copy className='w-4 h-4' />
+							)}
 						</Button>
 					</div>
 
 					<Button
+						onClick={onNew}
+						disabled={isLoading}
 						variant='link'
 						size='sm'
 						className='text-xs text-zinc-500 mt-4'
